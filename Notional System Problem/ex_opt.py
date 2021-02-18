a3ce1404c56515559bee0f8f993802cf44207f9d
Daniel Hulse <hulsed@oregonstate.edu>
2021-02-18 14:15:35 -0800

    Adding notional example optimization

--- Notional System Problem/ex_opt.py
+++ Notional System Problem/ex_opt.py
@@ -12,6 +12,7 @@ import time
 import itertools
 import random
 from scipy.optimize import minimize
+from scipy.optimize import NonlinearConstraint
 
 xdes = [1,1,1,1]
 xdb = ((0,100),(0,100),(0,100),(0,100))
@@ -29,7 +30,21 @@ def des_h_const(xdes):
 def des_g_const(xdes):
     return np.power(10.0, -xdes[1]) - xdes[2]
 
-constraints = [{'type':'eq', 'fun':des_h_const}, {'type':'ineq', 'fun':des_g_const}]
+
+h_con = NonlinearConstraint(des_h_const,0,0)
+g_con = NonlinearConstraint(des_g_const,0,np.inf)
+
+constraints = [{'type':'eq', 'fun': lambda x: x[0]-(x[3]+x[1]), 'jac': lambda x: np.array([1,-1,0,-1])}, {'type':'ineq', 'fun': lambda x: np.power(10.0, -x[1]) - x[2], 'jac': lambda x: np.array([0, -np.log(10)*np.power(10.0, -x[1]), -1, 0]) }]
 result = minimize(des_cost, xdes, method='SLSQP', bounds = xdb, constraints =constraints, args = const)
+
+
+# substituting in the h-constraint: x_s = x_p + x_a / x[3] = x[0] + x[1]
+#def des_cost_sub(xdes,const):
+#    return -const[0]*xdes[0] + const[1]*(xdes[0] + xdes[1]) + const[2]* np.power(10.0, xdes[2])
+#xdes_sub = [1,1,1]
+#xdb_sub = ((0,100),(0,100),(0,100))
+#
+#constraints_sub = [{'type':'ineq', 'fun': lambda x: np.power(10.0, -x[1]) - x[2], 'jac': lambda x: np.array([0, -np.log(10)*np.power(10.0, -x[1]), -1]) }]
+#result_sub = minimize(des_cost_sub, xdes_sub, method='trust-constr', bounds = xdb_sub, constraints =constraints_sub, args = const)
     
     
